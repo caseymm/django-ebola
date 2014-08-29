b15db3051661157ec5a159544c901b991adb7f48
caseymm <caseymm@gmail.com>
2014-08-29 04:29:05 -0400

    I promise I can do math.

--- liberia/management/commands/get_week_totals.py
+++ liberia/management/commands/get_week_totals.py
@@ -1,8 +1,10 @@
+from __future__ import division
 import json
 from liberia.models import SitRep, Location, LocationSitRep
 from django.core.management.base import BaseCommand
 import time
 from datetime import datetime
+
 
 class Command(BaseCommand):
     help = 'Get week totals for cases and deaths'
@@ -12,5 +14,20 @@ class Command(BaseCommand):
 
         today_total = LocationSitRep.objects.get(date='2014-08-27', location=national)
         week_ago_total = LocationSitRep.objects.get(date='2014-08-20', location=national)
-        print today_total.total_deaths_all - week_ago_total.total_deaths_all
-        print today_total.cases_cum - week_ago_total.cases_cum
+        deaths = today_total.total_deaths_all - week_ago_total.total_deaths_all
+        cases = today_total.cases_cum - week_ago_total.cases_cum
+        #
+        # print "% change"
+        # print (deaths/week_ago_total.total_deaths_all)*100
+        # print (cases/week_ago_total.cases_cum)*100
+
+        hcw_deaths = today_total.hcw_deaths_cum - week_ago_total.hcw_deaths_cum
+        hcw_cases = today_total.hcw_cases_cum - week_ago_total.hcw_cases_cum
+        print hcw_deaths
+
+
+        print "% change"
+        print (hcw_deaths/week_ago_total.hcw_deaths_cum)*100
+        print
+        print hcw_cases
+        print (hcw_cases/week_ago_total.hcw_cases_cum)*100
