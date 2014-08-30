c2467c00342357a3ff8ed62d1dff570ad61e5c06
caseymm <caseymm@gmail.com>
2014-08-30 18:02:40 -0400

    json for counties appears to be correct

--- liberia/views.py
+++ liberia/views.py
@@ -53,8 +53,14 @@ class LocationDetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super(LocationDetailView, self).get_context_data(**kwargs)
         context['location'] = self.object.locationsitrep_set.all()
-        context['location_vals'] = self.object.locationsitrep_set.all().values()
-        # context['loc_name'] = self.object.location
+        for ent in context['location']:
+            context['date_str'] = ent.date
+        context['list'] = []
+        context['location_vals'] = self.object.locationsitrep_set.values('location__name', 'total_deaths_probable', 'cases_cum_suspected', 'cases_cum_probable', 'cases_cum_confirmed', 'cases_cum', 'cases_new_total', 'cases_new_suspected', 'cases_new_probable', 'cases_new_confirmed', 'total_deaths_suspected', 'total_deaths_confirmed', 'total_deaths_all', 'deaths', 'new_deaths_probable', 'new_deaths_suspected', 'new_deaths_confirmed', 'hc_workers', 'hcw_cases_new', 'hcw_cases_cum', 'hcw_deaths_new', 'hcw_deaths_cum', 'CFR')
+
+        for i in context['location_vals']:
+            i.setdefault('date_str', context['date_str'])
+            context['list'].append(i)
         return context
 
     def render_to_response(self, context, **kwargs):
@@ -62,8 +68,7 @@ class LocationDetailView(generic.DetailView):
         format = self.request.GET.get('format', '')
         if 'json' in format:
             return HttpResponse(
-                json.dumps(context['location_vals'], default=smart_text),
-                content_type='application/json'
+                json.dumps(context['list'])
             )
 
         # And if it's none of the above return something normal
