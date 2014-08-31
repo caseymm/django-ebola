bfdb95797d3c474e2a51341dbd2901059c9c1de1
caseymm <caseymm@gmail.com>
2014-08-31 17:22:56 -0400

    got filtered list, but slow

--- liberia/views.py
+++ liberia/views.py
@@ -21,16 +21,31 @@ class LocationDetailView(generic.DetailView):
     context_object_name = 'loc'
 
     def get_context_data(self, **kwargs):
+        latest_date = SitRep.objects.latest('formatted_date')
+        nums=[latest_date.day_of_year]
+        num=latest_date.day_of_year
+        while (num - 7) > 0:
+            for sr in SitRep.objects.all():
+                if sr.day_of_year == (num-7):
+                    nums.append(sr.day_of_year)
+            num=num-7
+
         context = super(LocationDetailView, self).get_context_data(**kwargs)
         context['location'] = self.object.locationsitrep_set.all()
         for ent in context['location']:
             context['date_str'] = ent.date
         context['list'] = []
-        context['location_vals'] = self.object.locationsitrep_set.values('location__name', 'location__slug', 'total_deaths_probable', 'cases_cum_suspected', 'cases_cum_probable', 'cases_cum_confirmed', 'cases_cum', 'cases_new_total', 'cases_new_suspected', 'cases_new_probable', 'cases_new_confirmed', 'total_deaths_suspected', 'total_deaths_confirmed', 'total_deaths_all', 'deaths', 'new_deaths_probable', 'new_deaths_suspected', 'new_deaths_confirmed', 'hc_workers', 'hcw_cases_new', 'hcw_cases_cum', 'hcw_deaths_new', 'hcw_deaths_cum', 'CFR')
+        context['filtered_list'] = []
+        context['location_vals'] = self.object.locationsitrep_set.values('sit_rep__day_of_year', 'sit_rep__date', 'location__name', 'location__slug', 'total_deaths_probable', 'cases_cum_suspected', 'cases_cum_probable', 'cases_cum_confirmed', 'cases_cum', 'cases_new_total', 'cases_new_suspected', 'cases_new_probable', 'cases_new_confirmed', 'total_deaths_suspected', 'total_deaths_confirmed', 'total_deaths_all', 'deaths', 'new_deaths_probable', 'new_deaths_suspected', 'new_deaths_confirmed', 'hc_workers', 'hcw_cases_new', 'hcw_cases_cum', 'hcw_deaths_new', 'hcw_deaths_cum', 'CFR')
 
         for i in context['location_vals']:
-            i.setdefault('date_str', context['date_str'])
             context['list'].append(i)
+
+        for i in context['location_vals']:
+            for n in nums:
+                if i['sit_rep__day_of_year'] == n:
+                    context['filtered_list'].append(i)
+
         return context
 
     def render_to_response(self, context, **kwargs):
