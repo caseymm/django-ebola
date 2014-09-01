4ecd8c2ec354b63c49e72a76d09a28f7a2dcfc90
caseymm <caseymm@gmail.com>
2014-08-31 23:31:39 -0400

    added percent change death to dict for each loc - just appends to each entry, but would be fine for our use

--- liberia/views.py
+++ liberia/views.py
@@ -39,6 +39,7 @@ class LocationDetailView(generic.DetailView):
         context['location_vals'] = self.object.locationsitrep_set.values('sit_rep__day_of_year', 'sit_rep__date', 'location__name', 'location__slug', 'total_deaths_probable', 'cases_cum_suspected', 'cases_cum_probable', 'cases_cum_confirmed', 'cases_cum', 'cases_new_total', 'cases_new_suspected', 'cases_new_probable', 'cases_new_confirmed', 'total_deaths_suspected', 'total_deaths_confirmed', 'total_deaths_all', 'deaths', 'new_deaths_probable', 'new_deaths_suspected', 'new_deaths_confirmed', 'hc_workers', 'hcw_cases_new', 'hcw_cases_cum', 'hcw_deaths_new', 'hcw_deaths_cum', 'CFR')
 
         for i in context['location_vals']:
+            i.setdefault('pct_change_death', self.object.death_pct_change)
             context['list'].append(i)
 
         for i in context['location_vals']:
@@ -61,7 +62,7 @@ class LocationDetailView(generic.DetailView):
             )
 
         return super(LocationDetailView, self).render_to_response(context, **kwargs)
-
+#
 # class PctChangeListView(generic.ListView):
 #     template = 'templates/home/pct_change.html'
 #     model = Location
@@ -70,14 +71,6 @@ class LocationDetailView(generic.DetailView):
 #     # def get_queryset(self):
 #     #     locations = Location.objects.all()
 #     #     return locations
-#
-#     def get_context_data(self, **kwargs):
-#         context = super(PctChangeTemplateView, self).get_context_data(**kwargs)
-#         context[''] = .objects.get()
-#         return context
-
-# class PctChangeTemplateView(generic.TemplateView):
-#     template = 'templates/home/pct_change.html'
 #
 #     def get_context_data(self, **kwargs):
 #         context = super(PctChangeTemplateView, self).get_context_data(**kwargs)
