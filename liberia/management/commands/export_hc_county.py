48078c8eb06db50173c460c9b4d5cf57fdf64a0c
caseymm <caseymm@gmail.com>
2014-09-03 22:31:12 -0400

    needs to be entirely rewritten to get weekly info

--- liberia/management/commands/export_hc_county.py
+++ liberia/management/commands/export_hc_county.py
@@ -15,39 +15,81 @@ class Command(BaseCommand):
         county_json=open('latest_data/highcharts_county.json','w')
         # print "county deaths info"
 
-        county_d = 'series:['
-        new_deaths = {}
-        for obj in LocationSitRep.objects.filter(sit_rep=latest_qs).exclude(location=national).order_by('location').values('total_deaths_suspected', 'total_deaths_probable', 'total_deaths_confirmed'):
-            for attr in obj:
-                new_deaths.setdefault(attr, []).append(obj[attr])
-            #     print obj[attr]
+        # county_d = 'series:['
+        # new_deaths = {}
+        # for obj in LocationSitRep.objects.filter(sit_rep=latest_qs).exclude(location=national).order_by('location').values('total_deaths_suspected', 'total_deaths_probable', 'total_deaths_confirmed'):
+        #     for attr in obj:
+        #         new_deaths.setdefault(attr, []).append(obj[attr])
+        #     #     print obj[attr]
+        #
+        # for i in new_deaths:
+        #     county_d += '{'
+        #     county_d += 'name: '+i+','
+        #     county_d += 'data: '+str(new_deaths[i])
+        #     county_d += '},'
+        # county_d += ']'
+        #
+        # print>>county_json, county_d.replace(',]',']')
+        # print>>county_json
+        #
+        # # print "county cases info"
+        #
+        # county_c = 'series:['
+        # new_cases = {}
+        # for obj in LocationSitRep.objects.filter(sit_rep=latest_qs).exclude(location=national).order_by('location').values('cases_cum_suspected', 'cases_cum_probable', 'cases_cum_confirmed'):
+        #     for attr in obj:
+        #         new_cases.setdefault(attr, []).append(obj[attr])
+        #     #     print obj[attr]
+        #
+        # for i in new_cases:
+        #     county_c += '{'
+        #     county_c += 'name: '+i+','
+        #     county_c += 'data: '+str(new_cases[i])
+        #     county_c += '},'
+        # county_c += ']'
+        #
+        # # print county_c.replace(',]',']')
+        # print>>county_json, county_c.replace(',]',']')
+        # county_json.close()
 
-        for i in new_deaths:
-            county_d += '{'
-            county_d += 'name: '+i+','
-            county_d += 'data: '+str(new_deaths[i])
-            county_d += '},'
-        county_d += ']'
+        latest = SitRep.objects.latest('formatted_date')
+        nums = [int(latest.day_of_year)]
+        num=int(latest.day_of_year)
+        # print num
+        while (num - 7) > 0:
+            nums.append(num-7)
+            num=num-7
+        #     print num
+        # print nums
 
-        print>>county_json, county_d.replace(',]',']')
-        print>>county_json
+        weekly_deaths = []
+        weekly_cases = []
+        for obj in LocationSitRep.objects.filter(location=national).order_by('formatted_date'):
+            if obj.date:
+                d = datetime.strptime(obj.date, '%Y-%m-%d')
+                doy = datetime.strftime(d, "%j")
+                if int(doy) in nums:
+                    weekly_deaths.append(obj.deaths)
+                    weekly_cases.append(obj.cases_new_total)
 
-        # print "county cases info"
-
-        county_c = 'series:['
+        county_cd = 'series:['
         new_cases = {}
-        for obj in LocationSitRep.objects.filter(sit_rep=latest_qs).exclude(location=national).order_by('location').values('cases_cum_suspected', 'cases_cum_probable', 'cases_cum_confirmed'):
+        loc_query = LocationSitRep.objects.filter(sit_rep=latest_qs).exclude(location=national).order_by('location').values(
+        'cases_cum_suspected', 'cases_cum_probable', 'cases_cum_confirmed', 'total_deaths_suspected', 'total_deaths_probable',
+         'total_deaths_confirmed')
+        for obj in loc_query:
+            # obj['weekly_deaths'] = weekly_deaths
+            # obj['weekly_cases'] = weekly_cases
             for attr in obj:
                 new_cases.setdefault(attr, []).append(obj[attr])
-            #     print obj[attr]
 
         for i in new_cases:
-            county_c += '{'
-            county_c += 'name: '+i+','
-            county_c += 'data: '+str(new_cases[i])
-            county_c += '},'
-        county_c += ']'
+            county_cd += '{'
+            county_cd += 'name: '+i+','
+            county_cd += 'data: '+str(new_cases[i])
+            county_cd += '},'
+        county_cd += ']'
 
-        # print county_c.replace(',]',']')
-        print>>county_json, county_c.replace(',]',']')
-        county_json.close()
+        print county_cd.replace(',]',']')
+        # print>>county_json, county_cd.replace(',]',']')
+        # county_json.close()
