ad00feb5531ad13f1261bdf78bac9202de44ab23
caseymm <caseymm@gmail.com>
2014-09-08 02:01:13 -0400

    who knows

--- ebola_project/settings/base.py
+++ ebola_project/settings/base.py
@@ -265,7 +265,8 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
