4069ae348536c110359c42361b7570f1694a6619
caseymm <caseymm@gmail.com>
2014-09-07 20:13:21 -0400

    appending, but forgot to get difference

--- liberia/management/commands/get_new_weekly.py
+++ liberia/management/commands/get_new_weekly.py
@@ -9,9 +9,21 @@ class Command(BaseCommand):
     help = 'Get totals for new weekly deaths and new weekly cases and save to WeekOfYear obj.'
 
     def handle(self, *args, **options):
-        # for week in WeekOfYear.objects.all():
-        #     for sr in week.sitrep_set.all():
-        #         print sr
-        for group in LocationSitRep.objects.values('location__name', 'sit_rep__week_of_year__week'
-        ).annotate(max_deaths=Max("total_deaths_all")).order_by('location__name'):
-            print group
+        # for loc in Location.objects.all():
+        #     print loc
+
+        loc_dict = {}
+        for loc in Location.objects.all():
+            both = {}
+            deaths_array = []
+            cases_array = []
+            for entry in LocationSitRep.objects.values('location__name', 'sit_rep__week_of_year__week'
+            ).annotate(max_deaths=Max("total_deaths_all"), max_cases=Max("cases_cum")).order_by('location__name'):
+                if loc.name == entry['location__name']:
+                    deaths_array.append(entry['max_deaths'])
+                    cases_array.append(entry['max_cases'])
+                    both.setdefault('weekly_deaths', deaths_array)
+                    both.setdefault('weekly_cases', cases_array)
+                    loc_dict.setdefault(entry['location__name'], both)
+
+        print loc_dict
