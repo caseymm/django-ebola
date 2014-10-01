4798c5a888ac7cfec02f6e963dc136bcfb957c34
caseymm <caseymm@gmail.com>
2014-09-30 21:28:09 -0400

    revert settings

--- ebola_project/settings/base.py
+++ ebola_project/settings/base.py
@@ -44,7 +44,16 @@ MANAGERS = ADMINS
 
 ########## DATABASE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ebola',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 ########## END DATABASE CONFIGURATION
 
 
